3102417cf3c4f4d3278b2ff5cebfcf50b1e6390f
Atul <atul22g2468@gmail.com>
2024-09-25 02:45:30 +0530

    add vault

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,29 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import TypeDefs from './GraphQL/TypeDefs';
 import Resolvers from './GraphQL/Resolvers';
+import { ApolloServerPluginLandingPageLocalDefault, ApolloServerPluginLandingPageProductionDefault } from '@apollo/server/plugin/landingPage/default';
 
 const server = async () => {
-// Create A Apolo Server
-const server = new ApolloServer({
-    typeDefs: TypeDefs,
-    resolvers: Resolvers,
-});
+    // Create A Apolo Server
+    const server = new ApolloServer({
+        typeDefs: TypeDefs,
+        resolvers: Resolvers,
+        plugins: [
+            // Install a landing page plugin based on NODE_ENV
+            process.env.NODE_ENV === 'production'
+                ? ApolloServerPluginLandingPageProductionDefault({
+                    graphRef: 'my-graph-id@my-graph-variant',
+                    footer: false,
+                })
+                : ApolloServerPluginLandingPageLocalDefault({ footer: false }),
+        ],
+    });
 
-// Start The Apolo Server
-const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000 },
-});
-console.log(`🚀  Server ready at: ${url}`);
+    // Start The Apolo Server
+    const { url } = await startStandaloneServer(server, {
+        listen: { port: 4000 },
+    });
+    console.log(`🚀  Server ready at: ${url}`);
 }
 
 server();
