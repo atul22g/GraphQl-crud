42f978fa3016b62003a713717fc01eddf06d712c
Atul <atul22g2468@gmail.com>
2024-09-25 23:32:32 +0530

    add env

--- client/src/index.js
+++ client/src/index.js
@@ -6,8 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
-  // uri: process.env.REACT_APP_BACKEND_URL,
-  uri: "https://graphql-crud-backend.onrender.com/graphql",
+  uri: process.env.REACT_APP_BACKEND_URL,
   cache: new InMemoryCache(),
 });
 
