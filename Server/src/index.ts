11e7c85aa7b8f0f87efe43a29a852729ae150171
Atul <atul22g2468@gmail.com>
2024-09-25 16:19:35 +0530

    add cors

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -2,7 +2,7 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import TypeDefs from './GraphQL/TypeDefs';
 import Resolvers from './GraphQL/Resolvers';
-import { ApolloServerPluginLandingPageLocalDefault, ApolloServerPluginLandingPageProductionDefault } from '@apollo/server/plugin/landingPage/default';
+import cors from 'cors';
 
 const server = async () => {
     // Create A Apolo Server
@@ -11,9 +11,14 @@ const server = async () => {
         resolvers: Resolvers,
     });
 
-    // Start The Apolo Server
+    // Start The Apolo Server with CORS enabled
     const { url } = await startStandaloneServer(server, {
         listen: { port: 4000 },
+        context: async ({ req, res }) => {
+            // Apply CORS middleware with specific origin
+            cors({ origin: '*' })(req, res, () => {});
+            return { req, res };
+        },
     });
     console.log(`🚀  Server ready at: ${url}`);
 }
